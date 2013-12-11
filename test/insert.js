432dd9344ee5a35d73627253257ccec357d68c6f
James Kyburz <james.kyburz@gmail.com>
2013-12-11 11:36:17 +0100

    Added getComputedStyle polyfill to test - showing insert-css fails in ie8

--- test/insert.js
+++ test/insert.js
@@ -1,36 +1,38 @@
 var test = require('tape');
 var insertCss = require('../');
+var computedStyle = require('computed-style');
 
 var css = 'body { background-color: purple; color: yellow; }';
 
 test(function (t) {
     t.plan(6);
-    if (typeof window.getComputedStyle !== 'function') {
-        t.fail('cannot getComputedStyle()');
-    }
     
     var before = colors();
     t.ok(before.bg === 'rgba(0,0,0,0)' || before.bg === 'transparent');
-    t.equal(before.fg, 'rgb(0,0,0)');
+    t.ok(before.fg === 'rgb(0,0,0)' || before.fg === '#000000');
     
     insertCss(css);
     
     var after = colors();
-    t.equal(after.bg, 'rgb(128,0,128)');
-    t.equal(after.fg, 'rgb(255,255,0)');
+    t.ok(after.bg === 'rgb(128,0,128)' || after.bg === 'purple');
+    t.ok(after.fg === 'rgb(255,255,0)' || after.fg === 'yellow');
 
-    var resetStyle = 'body { background-color: transparent; color: black; }';
+    var resetStyle = 'body { background-color: transparent; color: #000000; }';
     insertCss(resetStyle);
 
     var reset = colors();
     t.ok(reset.bg === 'rgba(0,0,0,0)' || reset.bg === 'transparent');
-    t.ok(reset.fg === 'rgb(0,0,0)');
+    t.ok(reset.fg === 'rgb(0,0,0)' || reset.fg === '#000000');
 });
 
 function colors () {
-    var style = window.getComputedStyle(document.body);
+    var body = document.getElementsByTagName('body')[0];
     return {
-        bg: style.backgroundColor.replace(/\s+/g, ''),
-        fg: style.color.replace(/\s+/g, '')
+      fg: style('color'),
+      bg: style('background-color')
     };
+
+    function style(attr) {
+      return computedStyle(body, attr).replace(/\s/g, '');
+    }
 }
